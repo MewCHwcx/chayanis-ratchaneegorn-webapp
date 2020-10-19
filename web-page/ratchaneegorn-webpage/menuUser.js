fe2fbf0bdc544fffa134324575c9ce9b4b5d8b15
ratchaneegorn <s.ratchaneegron@kkumail.com>
2020-10-19 23:09:43 +0700

    project-process

--- web-page/ratchaneegorn-webpage/menuUser.js
+++
@@ -1,8 +0,0 @@
-    
-    let p1 = document.querySelector("p")
-    p1.addEventListener("click", changename)
-
-    function changename(){
-        let name = prompt("enter a name")
-        p1.textContent = 'logout' + name;
-    }
